9b66ebeda795eac17ab7dcd7d9dec17e8dfa5019
Emre <144052970+emreuyanik@users.noreply.github.com>
2024-03-15 21:30:08 +0300

    up

--- Server/startup/mongooseConnection.js
+++ Server/startup/mongooseConnection.js
@@ -1,11 +1,13 @@
 "use strict"
 
 const mongoose = require("mongoose");
-
+console.log(process.env.MONGODB_URI);
+console.log("🚀 ~ process.env.MONGODB_URI:", process.env.MONGODB_URI)
 const mongooseConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("db connected");
+
   } catch (error) {
     console.log("db not connected");
     throw new Error("db connection is failed");
